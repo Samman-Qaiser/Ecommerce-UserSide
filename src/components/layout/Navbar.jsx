738f9f9b76e9df74bab49d0654c526d5e183ef97
Samman Qaiser <sammansheikh6@gmail.com>
2026-02-02 15:37:19 +0500

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Input } from '@/components/ui/input';
-import { menuData } from '@/data/menuData';
+import { menuData } from '../../data/menudata';
 import { cn } from '@/lib/utils';
 import {
   Dialog,
